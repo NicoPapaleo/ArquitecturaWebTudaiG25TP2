682a890cc6924d6ca29364fdc20ee4684ff761ca
Nico Papaleo <nikoopapaleo@gmail.com>
2024-09-23 17:23:26 -0300

    Creacion de metodo findByEnrolledStudentsOrderedByCant

--- src/main/java/entity/Career.java
+++ src/main/java/entity/Career.java
@@ -7,10 +7,13 @@ import java.util.List;
 @Entity
 @NamedQuery(name= Career.BUSCAR_TODAS, query=" SELECT c FROM  Career c")
 @NamedQuery(name= Career.EXIST_CAREER, query=" SELECT c FROM  Career c WHERE idCareer = :idCareer")
+@NamedQuery(name= Career.BUSCAR_ESTUDIANTES_ORDENADOS, query="SELECT c.name, COUNT(e) AS cant_insctiptos FROM Career c JOIN Courses.student e GROUP BY c.name HAVING COUNT(e)>0 ORDER BY cant_insctiptos DESC")
+
 public class Career {
 
     public static final String EXIST_CAREER = "Career.EXIST_CAREER";
     public static final String BUSCAR_TODAS = "Career.BUSCAR_TODAS";
+    public static final String BUSCAR_ESTUDIANTES_ORDENADOS = "Career.BUSCAR_ESTUDIANTES_ORDENADOS";
 
     @Id
     //@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/repository/implementsRepositories/CareerRepositoryImp.java
+++ src/main/java/repository/implementsRepositories/CareerRepositoryImp.java
@@ -1,5 +1,6 @@
 package repository.implementsRepositories;
 
+import dto.CareerDTO;
 import entity.Career;
 import entity.Student;
 import repository.helper.EntityManagerHelper;
@@ -28,5 +29,25 @@ public class CareerRepositoryImp extends BaseJPARepository<Career, Integer> {
             em.close();
         }
         return ls;
+    }
+
+    /**
+     * @brief busca las carreras con estudiantes inscriptos ordenados por cantidad
+     * @return retorna una lista de carreras ordenada por cantidad
+     */
+    public List<CareerDTO> findByEnrolledStudentsOrderedByCant() {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        List<CareerDTO> lc =new ArrayList<>();
+        try {
+            Query query = em.createNamedQuery(Career.BUSCAR_ESTUDIANTES_ORDENADOS, CareerDTO.class);
+            lc = query.getResultList();
+        }
+        catch(Exception e){
+            throw new RuntimeException("Error en la consulta"+e);
+        }
+        finally{
+            em.close();
+        }
+        return lc;
     }
 }
